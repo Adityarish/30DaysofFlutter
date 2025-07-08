7aeaefd392c0cd28574e59494a0f457786a6377d
Adityarish <seemeadit21824@gmail.com>
2025-07-08 13:01:05 +0530

    day 6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // if a variable stated with '_' then it is private
+  // state means the data that is changing over time
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,7 +28,7 @@ class LoginPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
@@ -35,6 +44,10 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter username",
                       labelText: "Username",
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {}); // this is only used in statefullwidget
+                    },
                   ),
                   TextFormField(
                     obscureText: true, // defined before the text
@@ -44,20 +57,63 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 20),
-                  ElevatedButton(
-                    child: Text("Login", style: TextStyle(fontSize: 20)),
-                    onPressed: () {
+                  InkWell(
+                    // used to make the widget clickable
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+
+                      await Future.delayed(Duration(milliseconds: 500));
                       Navigator.pushNamed(
                         context,
                         MyRoutes.homeRoute,
-                      ); //Navigating to the home page uses Navigator class # push mtlb aage jana pop mtlb piche aana
-                      print("Logged in Successfully");
+                      ); // push mean next page pop means back page
                     },
-                    style: TextButton.styleFrom(
-                      minimumSize: Size(180, 50),
-                      // foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    child: AnimatedContainer(
+                      // container is used to create a box  but not ans animation
+                      duration: Duration(milliseconds: 500),
+                      width: changeButton ? 50 : 180,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(Icons.done)
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(
+                          255,
+                          62,
+                          199,
+                          137,
+                        ), // we cann't provide decoration and container color both
+                        // shape: changeButton ? BoxShape.circle: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(
+                          changeButton ? 50 : 8,
+                        ),
+                      ),
                     ),
                   ),
+                  // ElevatedButton(
+                  //   child: Text("Login", style: TextStyle(fontSize: 20)),
+                  //   onPressed: r() {
+                  //     Navigator.pushNamed(
+                  //       context,
+                  //       MyRoutes.homeRoute,
+                  //     ); //Navigating to the home page uses Navigator class # push mtlb aage jana pop mtlb piche aana
+                  //     print("Logged in Successfully");
+                  //   },
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(180, 50),
+                  //     // foregroundColor: WidgetStatePropertyAll(Colors.white),
+                  //   ),
+                  // ),
                 ],
               ),
             ),
